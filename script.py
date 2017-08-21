b27c289ad52b31d892bbb9dd93e1b69b24fe742b
AnonymerNiklasistanonym <niklas.mikeler@gmail.com>
2017-08-21 19:18:19 +0200

    Added logging and updated the SimplifiedGmailApi submodule

--- script.py
+++ script.py
@@ -12,10 +12,12 @@ import codecs
 from datetime import datetime
 import difflib
 import io
+import logging
+
 
 # Gmail API - set true if you want to use the SimplifiedGmailApi
 # comment the import if you don't need it
-from SimplifiedGmailApiSubmodule.SendGmailSimplified import SimplifiedGmailApi 
+from SimplifiedGmailApiSubmodule.SendGmailSimplified import SimplifiedGmailApi
 GMAIL_API = False
 
 # Paths for important directories and files - from home directory
@@ -31,6 +33,9 @@ DIR_OF_DOWNLOADS = os.path.join(DIR_OF_SCRIPT, "Downloads")
 
 # other things
 JSON_TAG_MODIFIED_TIME = "last-modified-time"
+
+# Logging file
+logging.basicConfig(filename=os.path.join(DIR_OF_SCRIPT, "script.log"), level=logging.DEBUG)
 
 
 if GMAIL_API:
@@ -76,34 +81,42 @@ try:
     print("Login to " + credentials["host-address"] + " with " + credentials["username"] + ":")
     ftp = FTP_TLS(credentials["host-address"])
     ftp.login(user=credentials["username"], passwd=credentials["password"])
+    logging.info("Successfully logged in to " + credentials["host-address"] + " with " + credentials["username"])
 # exceptions if the connection cannot be established
 except ftplib.error_reply as e:
     # Exception raised when an unexpected reply is received from the server.
     print(e)
+    logging.warning(e)
     quit()
 except ftplib.error_temp as e:
     # hi
     # Exception raised when an error code signifying a temporary error (response codes in the range 400 - 499) is received.
     print(e)
+    logging.warning(e)
     quit()
 except ftplib.error_perm as e:
     # Exception raised when an error code signifying a permanent error (response codes in the range 500 - 599) is received.
     print(e)
+    logging.warning(e)
     quit()
 except ftplib.error_proto as e:
     # Exception raised when a reply is received from the server that does not fit the response specifications of the File Transfer Protocol, i.e. begin with a digit in the range 1 - 5.
     print(e)
+    logging.warning(e)
     quit()
 except ftplib.all_errors as e:
     # Other exceptions.
     print(e)
+    logging.warning(e)
     quit()
 
 
 # now download and compare each file in the JSON file
 for ftp_file in ftp_files:
 
-    print(">> Check the file " + ftp_file["path"])
+    info_text = ">> Check the file " + ftp_file["path"]
+    print(info_text)
+    logging.info(info_text)
 
     # get modified date from the current ftp file:
     # thanks to: https://stackoverflow.com/questions/20049928/created-date-of-file-via-ftp
@@ -122,7 +135,9 @@ for ftp_file in ftp_files:
     if not os.path.exists(CURRENT_LOCAL_PATH_MODIFIED_TIME):
 
         # save date and file because it's the first time
-        print("Change detected: First time check")
+        info_text = "Change detected: First time check"
+        print(info_text)
+        logging.info(info_text)
         save_string_to_json(new_last_modified_time[JSON_TAG_MODIFIED_TIME], CURRENT_LOCAL_PATH_MODIFIED_TIME)
         file_modification_detected = True
     else:
@@ -133,13 +148,17 @@ for ftp_file in ftp_files:
 
         # if date is another one overwrite all date and save new date
         if current_last_modified_time[JSON_TAG_MODIFIED_TIME] != new_last_modified_time[JSON_TAG_MODIFIED_TIME]:
-            print("Change detected: File was modified: new: " + new_last_modified_time[JSON_TAG_MODIFIED_TIME] + " | old: " + current_last_modified_time[JSON_TAG_MODIFIED_TIME])
+            info_text = ("Change detected: File was modified: new: " + new_last_modified_time[JSON_TAG_MODIFIED_TIME] + " | old: " + current_last_modified_time[JSON_TAG_MODIFIED_TIME])
+            print(info_text)
+            logging.info(info_text)
             save_string_to_json(new_last_modified_time["last-modified-time"], CURRENT_LOCAL_PATH_MODIFIED_TIME)
             file_modification_detected = True
 
         # if the date is the same do nothing
         else:
-            print("No change detected")
+            info_text = "No change detected"
+            print(info_text)
+            logging.info(info_text)
             file_modification_detected = False
 
     # if ftp file is a text file and a change was detected
@@ -151,7 +170,9 @@ for ftp_file in ftp_files:
             CURRENT_LOCAL_PATH_OLD = os.path.join(DIR_OF_DOWNLOADS, ftp_file["id"] + "_old" + ".txt")
 
             # download the file via ftp and save it locally
-            print('Getting ' + ftp_file["path"] + " (id=" + ftp_file["id"] + ")")
+            info_text = ('Getting ' + ftp_file["path"] + " (id=" + ftp_file["id"] + ")")
+            print(info_text)
+            logging.info(info_text)
             local_file = open(CURRENT_LOCAL_PATH_NEW, 'wb')
             ftp.retrbinary('RETR ' + ftp_file["path"], local_file.write, 1024)
             local_file.close()
@@ -190,12 +211,20 @@ for ftp_file in ftp_files:
                 print(line.strip("\n"))
                 email_text += line.strip("\n") + "\n"
 
-            if GMAIL_API:
-                # Gmail API - Uncomment the coming lines (2,4) if you want to use the Simplified Gmail API
-                # Send email:
-                subject = "Change of the file " + ftp_file["id"] + " (" + ftp_file["path"] + ")"
-                text = email_text + "\n\n(" + new_last_modified_time["last-modified-time"] + ")"
-                GmailServer.send_plain(credentials["email-if-change"], subject, text)
+            try:
+                if GMAIL_API:
+                    # Gmail API - Uncomment the coming lines (2,4) if you want to use the Simplified Gmail API
+                    # Send email:
+                    subject = "Change of the file " + ftp_file["id"] + " (" + ftp_file["path"] + ")"
+                    text = email_text + "\n\n(" + new_last_modified_time["last-modified-time"] + ")"
+                    GmailServer.send_plain(credentials["email-if-change"], subject, text)
+            except UnicodeEncodeError as e:
+                if GMAIL_API:
+                    # Gmail API - Uncomment the coming 3 lines if you want to use the Simplified Gmail API
+                    # Send email:
+                    subject = "Change of the file " + ftp_file["id"] + " (" + ftp_file["path"] + ")"
+                    text = "Last modified time: " + new_last_modified_time["last-modified-time"]
+                    GmailServer.send_plain(credentials["email-if-change"], subject, text)
 
         else:
             print("")
